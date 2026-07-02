baef5417511176ab8f8ab05cc6ae53cea0ded577
Koster410 <k87305444@gmail.com>
2026-07-02 20:43:33 +0500

    .

---
+++ Vim/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Установка Vim-конфига с GitHub
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp .vimrc ~/.vimrc
+vim +PlugInstall +qall
